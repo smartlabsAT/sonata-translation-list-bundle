85d7fa28a525672fd558dfaef1da56c9d3837af1
Christopher Schwarz <christopher@neugebauerschwarz.com>
2026-04-02 17:35:23 +0200

    refactor: migrate to AbstractBundle, remove legacy DI classes
    Replace legacy Bundle + Extension + Configuration pattern with Symfony 7.x
    AbstractBundle. Configuration tree, service loading, and asset prepending
    now live directly in the bundle class. Directory structure modernized:
    src/Resources/{config,views,public} moved to {config,templates,public}.

--- src/SonataTranslationListBundle.php
+++ src/SonataTranslationListBundle.php
@@ -4,13 +4,51 @@ declare(strict_types=1);
 
 namespace Smartlabs\SonataTranslationListBundle;
 
-use Smartlabs\SonataTranslationListBundle\DependencyInjection\SonataTranslationListExtension;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class SonataTranslationListBundle extends Bundle
+class SonataTranslationListBundle extends AbstractBundle
 {
-    protected function getContainerExtensionClass(): string
+    public function configure(DefinitionConfigurator $definition): void
     {
-        return SonataTranslationListExtension::class;
+        $definition->rootNode()
+            ->children()
+                ->arrayNode('locales')
+                    ->defaultValue(['de', 'en'])
+                    ->scalarPrototype()->end()
+                    ->info('List of locales available for translation.')
+                ->end()
+                ->scalarNode('default_locale')
+                    ->defaultValue('%kernel.default_locale%')
+                    ->info('The default (source) locale.')
+                ->end()
+            ->end()
+        ;
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $builder->setParameter('sonata_translation_list.locales', $config['locales']);
+        $builder->setParameter('sonata_translation_list.default_locale', $config['default_locale']);
+
+        $container->import('../config/services.php');
+    }
+
+    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        if ($builder->hasExtension('sonata_admin')) {
+            $builder->prependExtensionConfig('sonata_admin', [
+                'assets' => [
+                    'extra_javascripts' => [
+                        'bundles/sonatatranslationlist/js/translation-list.js',
+                    ],
+                    'extra_stylesheets' => [
+                        'bundles/sonatatranslationlist/css/translation-list.css',
+                    ],
+                ],
+            ]);
+        }
     }
 }
